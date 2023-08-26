91061cf63b3a188078dc7a0df8f5435cd5959f4f
Sean Slicer <722sas17@gmail.com>
2023-08-26 04:50:14 -0500

    auth validation schema added

---
+++ src/validation/auth/auth.ts
@@ -0,0 +1,22 @@
+import * as z from "zod";
+
+const specialCharacters = /[ `!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/
+
+export const loginSchemaEmail= z.object({
+  email: z.string().email(),
+  password: z.string() 
+  .regex(new RegExp(".*[A-Z].*"), "One uppercase character")
+  .regex(new RegExp(".*\\d.*"), "One number")
+  .min(8, "Must be at least 8 characters in length"),
+});
+
+export const signUpSchema = loginSchemaEmail.extend({
+  username: z.string().min(3, "Username must me greater than 3 characters").max(25, "Username must be under 25 characters").refine(value => specialCharacters.test(value), {message: "Username should not contain special characters"})
+  ,
+});
+
+export const loginSchemaUsername = signUpSchema.omit({email: true});
+
+export type LoginEmail = z.infer<typeof loginSchemaEmail>;
+export type LoginUsername = z.infer<typeof loginSchemaUsername>;
+export type SignUp = z.infer<typeof signUpSchema>;
