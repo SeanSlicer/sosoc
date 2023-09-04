4bf95fb67eeb8506067f500dc1fe016686406fdc
Sean Slicer <722sas17@gmail.com>
2023-09-04 12:36:09 -0500

    auth provider change

--- env.js
+++ env.js
@@ -7,6 +7,9 @@ export const env = createEnv({
     NODE_ENV: z.enum(["development", "test", "production"]),
     // Data
     DATABASE_URL: z.string().min(1),
+    //Auth
+    GOOGLE_CLIENT_ID: z.string().min(1),
+    GOOGLE_CLIENT_SECRET:z.string().min(1),
     // AWS
     AWS_REGION: z.string().min(1),
     BUCKET_NAME: z.string().min(1),
@@ -21,6 +24,9 @@ export const env = createEnv({
     NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
     // Data
     DATABASE_URL: process.env.DATABASE_URL,
+    //Auth
+    GOOGLE_CLIENT_ID:  process.env.GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
     // AWS
     AWS_ACCESS_KEY: process.env.AWS_ACCESS_KEY,
     AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
