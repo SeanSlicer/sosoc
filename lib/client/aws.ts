57fe44fc64bf7a4988ce889848d72937b2e16b4b
Sean Slicer <722sas17@gmail.com>
2023-09-05 00:58:22 -0500

    aws access key id name change (for vercel)

--- lib/client/aws.ts
+++ lib/client/aws.ts
@@ -2,13 +2,13 @@ import * as AWS from "aws-sdk"
 import { env } from "../../src/env.mjs"
 
 export const S3 = new AWS.S3({
-  accessKeyId: env.AWS_ACCESS_KEY,
+  accessKeyId: env.AWS_ACCESS_KEY_ID,
   secretAccessKey: env.AWS_SECRET_ACCESS_KEY,
   region: env.AWS_REGION,
 })
 
 export const SES = new AWS.SESV2({
-  accessKeyId: env.AWS_ACCESS_KEY,
+  accessKeyId: env.AWS_ACCESS_KEY_ID,
   secretAccessKey: env.AWS_SECRET_ACCESS_KEY,
   region: env.AWS_REGION,
 })
