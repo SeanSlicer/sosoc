bcb35221987dccc52730c15f3a92fd19483fb1d2
Sean Slicer <722sas17@gmail.com>
2023-09-05 03:19:35 -0500

    auth validation update

--- src/validation/auth/auth.ts
+++ src/validation/auth/auth.ts
@@ -1,6 +1,6 @@
-import * as z from "zod";
+import {z} from "zod";
 
-const specialCharacters = /[ `!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/
+const specialCharacters = /^[a-zA-Z0-9_]*$/
 
 export const loginSchemaEmail= z.object({
   email: z.string().email(),
@@ -11,8 +11,9 @@ export const loginSchemaEmail= z.object({
 });
 
 export const signUpSchema = loginSchemaEmail.extend({
-  username: z.string().min(3, "Username must me greater than 3 characters").max(25, "Username must be under 25 characters").refine(value => specialCharacters.test(value), {message: "Username should not contain special characters"})
-  ,
+  username: z.string().min(3, "Username must me greater than 3 characters").max(25, "Username must be under 25 characters").refine((value) => specialCharacters.test(value), {
+    message: "Username should not contain special characters",
+  }),
 });
 
 export const loginSchemaUsername = signUpSchema.omit({email: true});
