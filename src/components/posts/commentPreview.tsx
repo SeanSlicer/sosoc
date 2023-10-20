484f7f42791c42b9980e19f518722675b43988b6
Sean Slicer <722sas17@gmail.com>
2023-10-19 20:16:17 -0500

    components added for post

---
+++ src/components/posts/commentPreview.tsx
@@ -0,0 +1,7 @@
+import React, { FC } from "react"
+
+const CommentPreview : FC = (props) => {
+    return(<><div>wow</div></>);
+}
+
+export default CommentPreview
--- src/components/posts/post.tsx
+++ src/components/posts/post.tsx
@@ -1,8 +1,13 @@
 import React, { useState } from "react";
 import PostLike from "./postLike";
+import CommentPreview from "./commentPreview";
 
-const Post : React.FC = () => { 
-    return <PostLike />
+const Post : React.FC = (props : ) => { 
+    return (
+        <div>
+    <PostLike />
+    <CommentPreview />
+    </div>)
 }
 
 export default Post;
